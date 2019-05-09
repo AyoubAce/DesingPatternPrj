7a4407f7e5b1792a4ce6add50a893f62938754df
AyoubAce <36710490+AyoubAce@users.noreply.github.com>
2019-05-09 18:12:18 +0300

    Add files via upload

---
+++ CountDownCode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Text))]
+public class CountDownCode : MonoBehaviour
+{
+
+    Text countDown;
+    public delegate void CountDownDone();
+    public static event CountDownDone OnCountDownDone;
+
+    private void OnEnable()
+    {
+        countDown = GetComponent<Text>();
+        countDown.text = "3";
+        StartCoroutine("count_down");
+    }
+    IEnumerator count_down()
+    {
+        int i, count = 3;
+        for (i = 0; i < count; i++)
+        {
+            countDown.text = (count - i).ToString();
+            yield return new WaitForSeconds(1);
+        }
+        OnCountDownDone();
+    }
+
+}
